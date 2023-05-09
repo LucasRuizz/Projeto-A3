2337ce5cc2c651529c4c272732279fa7d8bce8ac
Lucas Ruiz <luquinhasribeiro7@gmail.com>
2023-05-08 22:24:26 -0300

    modificado

--- src/projeto_a3/Pesquisador.java
+++ src/projeto_a3/Pesquisador.java
@@ -6,11 +6,29 @@ public class Pesquisador extends Usuario{
     private int id_pesquisador;
     private boolean licenca;
     
-    public Pesquisador(int id_usuario, String nome, String telefone, String email) {
+    public Pesquisador(int id_usuario, String nome, String telefone, String email, int id_pesquisador, boolean licenca) {
         super(id_usuario, nome, telefone, email);
         this.id_pesquisador = id_pesquisador;
         this.licenca = licenca;
     }
+
+    public int getId_pesquisador() {
+        return id_pesquisador;
+    }
+
+    public void setId_pesquisador(int id_pesquisador) {
+        this.id_pesquisador = id_pesquisador;
+    }
+
+    public boolean isLicenca() {
+        return licenca;
+    }
+
+    public void setLicenca(boolean licenca) {
+        this.licenca = licenca;
+    }
+    
+    
     
     public void inserirDados(){
         
--- src/projeto_a3/Projeto_A3.java
+++ src/projeto_a3/Projeto_A3.java
@@ -1,10 +1,30 @@
 
 package projeto_a3;
 
+import java.util.Scanner;
+
 public class Projeto_A3 {
 
     public static void main(String[] args) {
   
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite seu id: ");
+        int id_usuario = sc.nextInt();
+        
+        System.out.println("Digite seu nome: ");
+        String nome = sc.next();
+        
+        System.out.println("Digite seu telefone: ");
+        String telefone = sc.next();
+        
+        System.out.println("Digite seu email: ");
+        String email = sc.next();
+        
+        Usuario user = new Usuario(id_usuario, nome, telefone, email);
+        System.out.printf("id: %d, nome: %s, telefone: %s, email: %s%n", 
+                user.getId_usuario(), user.getNome(), user.getTelefone(), user.getEmail());
+        
     }
     
 }
